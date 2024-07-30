b8396ce820cb953494902619974d96a6642976ed
tomas01vn <nguyendo76ngant@gmail.com>
2024-07-31 00:02:45 +0700

    old code 1

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ import ChipPoker from "./chip-poker";
 import Typography from "./typography";
 import Button from "./button";
 import Slot from "./slot";
-import BannerSlider from "./banner-slider";
 
-export { ChipPoker, Typography, Button, Slot, BannerSlider };
+export { ChipPoker, Typography, Button, Slot };
--- src/constants/global.const.ts
+++ src/constants/global.const.ts
@@ -3,34 +3,4 @@ const _GLOBAL = {
     TOKEN: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6Ijk4YzE3OWJhLTdmYTQtNGYxNi05ODIzLTRlM2RiNmY5M2NhMCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJva3BvayIsImp0aSI6IjBiODMyNmE0LWEzYjMtNDM5NS1iNTliLTlmNDdhYWM2Mjg5ZCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFnZW50IiwiZXhwIjoxNzIyMzI2NzU1LCJpc3MiOiJodHRwczovL2dvbGRlbmdhdGV4LmNvbS8iLCJhdWQiOiJodHRwczovL2dvbGRlbmdhdGV4LmNvbS8ifQ.g3JiD8A5CHTTREyvxm_CDQ2g-oT3QBabBTfbFB4c86Etf6S0c0V4vhRb85XLvyPfqgvOvGnIIysn-HTJkJqcFg'
 }
 
-const _IMAGE_URL = {
-    logoHeader: "/icons/logo.svg",
-    logo1: "/images/logo/logo1.png",
-    logo2: "/images/logo/logo2.png",
-    logo3: "/images/logo/logo3.png",
-    logo4: "/images/logo/logo4.png",
-    logo5: "/images/logo/logo5.png",
-    logo6: "/images/logo/logo6.png",
-    visa: "/images/logo/Visa.png",
-    mastercard: "/images/logo/Mastercard.png",
-    amex: "/images/logo/Amex.png",
-    discover: "/images/logo/Discover.png",
-    money1: "/images/logo/money1.png",
-    money2: "/images/logo/money2.png",
-    money3: "/images/logo/money3.png",
-    money4: "/images/logo/money4.png",
-    money5: "/images/logo/money5.png",
-    telegram: "/images/icon/Vector.png",
-    kakao: "/images/icon/headphones.png",
-    user: "/images/icon/user.png",
-    coin: "/images/icon/coin.png",
-}
-const _FLAG_COUNTRY = [
-    "/images/flags/vietnam.png",
-    "/images/flags/ame.png",
-    "/images/flags/china.png",
-    "/images/flags/japan.png",
-    "/images/flags/korea.png",
-]
-
-export { _GLOBAL, _IMAGE_URL, _FLAG_COUNTRY };
+export { _GLOBAL };
